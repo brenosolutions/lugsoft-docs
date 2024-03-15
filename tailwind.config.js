2032513c0eec17147bb91b22b4b01baf24c74abf
= <brenosofts@gmail.com>
2024-03-15 11:33:27 -0300

    refactor: configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ]
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
-
--- theme.config.tsx
+++ theme.config.tsx
@@ -3,20 +3,33 @@ import { useRouter } from 'next/router'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-
   useNextSeoProps() {
     const { asPath } = useRouter();
-    const titleTemplate = asPath !== '/' ? '%s – RádioBS' : 'RádioBS - Documentação'
+    const cleanPath = asPath.split('#')[0];
+    const directory = cleanPath.split('/')[1];
+    const titleTemplate = directory !== '' ? '%s – RádioBS' : 'Documentação - RádioBS';
     return { titleTemplate }
   },
 
+  head: () => {
+    return (
+      <meta httpEquiv="Content-Language" content='pt' />
+    )
+  },
+
   logo: <span><b>RádioBS</b> - V5.0.7</span>,
+
 
   project: {
     link: 'https://github.com/brenosolutions/lugsoft-docs',
   },
 
   docsRepositoryBase: 'https://github.com/brenosolutions/lugsoft-docs/tree/main/',
+
+  toc: {
+    title: "Nesta Página",
+    backToTop: true
+  },
 
   search: {
     placeholder: 'Pesquisar Documentação...',
@@ -30,9 +43,23 @@ const config: DocsThemeConfig = {
     content: 'Feedback'
   },
 
-  footer: {
-    text: 'Documentação RádioBS',
+  gitTimestamp: ({ timestamp }) => {
+    const month = timestamp.toLocaleString("default", { month: "long" })
+    const date = timestamp.getDate()
+    const year = timestamp.getFullYear()
+    return (<> Última atualização em {month} {date}, {year} </>)
   },
+
+  footer: {
+    text: () => {
+      return (
+        <span>
+          Desenvolvido por <a href={`https://www.lugsoft.com.br`} target="_blank" rel="noopener" className='font-bold'> Lugosft </a>
+        </span>
+      );
+    },
+  },
+
 
 }
 
