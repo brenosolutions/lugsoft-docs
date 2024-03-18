21404a803482160a11e34b11b378171abc11e304
Breno Costa <brenosofts@gmail.com>
2024-03-17 23:27:17 -0300

    refactor: config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 
+const {
+  RADIOBS_URL = 'https://radiobs.lugsoft.com.br:3001'
+} = process.env
+
 module.exports = {
   async redirects() {
     return [
@@ -9,6 +13,24 @@ module.exports = {
         destination: '/radiobs',
         permanent: false,
       },
+    ]
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `/:path*`,
+      },
+      // radiobs docs
+      {
+        source: '/radiobs',
+        destination: `${RADIOBS_URL}/radiobs`,
+      },
+      {
+        source: '/radiobs/:path*',
+        destination: `${RADIOBS_URL}/radiobs/:path*`,
+      }
     ]
   },
 }
