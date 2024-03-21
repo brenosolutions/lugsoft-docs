ec32af7d4cb625ec0e16bc4d2123aa9a71323e49
= <brenosofts@gmail.com>
2024-03-21 07:30:26 -0300

    docs: add troubleshooting

--- apps/radiobs/tailwind.config.js
+++ apps/radiobs/tailwind.config.js
@@ -3,9 +3,11 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./theme.config.tsx"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: 'class'
 }
